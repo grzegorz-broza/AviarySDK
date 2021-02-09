b4e9d11729e226f1b5a1f0a2db81c10520c5d49c
Grzegorz <grzegorz.broza@fotka.com>
2021-02-09 22:06:43 +0100

    second commit

---
+++ AviarySDK-4.4.5/AviarySDK/AviarySDK.framework/Versions/A/Headers/AviarySDK.h
@@ -0,0 +1,16 @@
+//
+//  AviarySDK.h
+//  AviarySDK
+//
+//  Copyright (c) 2014 Aviary. All rights reserved.
+//
+
+#import <AviarySDK/AVYPhotoEditorController.h>
+#import <AviarySDK/AVYInAppPurchaseManager.h>
+#import <AviarySDK/AVYOpenGLManager.h>
+#import <AviarySDK/AVYPhotoEditorContext.h>
+#import <AviarySDK/AVYPhotoEditorController.h>
+#import <AviarySDK/AVYPhotoEditorProduct.h>
+#import <AviarySDK/AVYPhotoEditorSession.h>
+#import <AviarySDK/AVYPhotoEditorCustomization.h>
+#import <AviarySDK/AVYPhotoEditorCompatibility.h>
